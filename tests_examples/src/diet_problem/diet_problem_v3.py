96820d6767737315960f5ab9551ca11d90fd88fb
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-08-03 17:37:03 -0300

    adding experiments to the library pulp

--- tests_examples/src/diet_problem/diet_problem_v3.py
+++ tests_examples/src/diet_problem/diet_problem_v3.py
@@ -1,5 +1,7 @@
 """
 Small example of the diet problem to learn how to use PuLP
+Using default PuLP solver
+Adding LpConstraint object
 
 Minimize: 20x1 + 10x2 + 16x3
 Subject to:
@@ -10,7 +12,7 @@ Subject to:
 """
 
 # imports
-from pulp import LpMinimize, LpProblem, LpVariable, LpStatus
+from pulp import LpMinimize, LpProblem, LpVariable, LpStatus, LpConstraint
 
 # model creation
 lp_model = LpProblem(name="diet-problem-test", sense=LpMinimize)
@@ -21,7 +23,11 @@ decision_var_y = LpVariable(name="y", lowBound=0)
 decision_var_z = LpVariable(name="z", lowBound=0)
 
 # constraints
-lp_model += 2 * decision_var_x + 3 * decision_var_y + 7 * decision_var_z >= 10
+constraint_1 = LpConstraint(
+    2 * decision_var_x + 3 * decision_var_y + 7 * decision_var_z >= 10
+)
+
+lp_model += constraint_1
 lp_model += 4 * decision_var_x + 2 * decision_var_y + 1 * decision_var_z >= 15
 lp_model += 1 * decision_var_x + 8 * decision_var_y + 1 * decision_var_z >= 10
 lp_model += 3 * decision_var_x + 1 * decision_var_y + 1 * decision_var_z >= 8
